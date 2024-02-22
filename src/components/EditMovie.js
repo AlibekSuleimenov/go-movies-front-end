799d08604ba0bd1abe351c20b09eab4078ee5e74
Alibek Suleimenov <alibek443@gmail.com>
2024-02-22 18:02:10 +0600

    added sweetalert pkg, finished the form to include genres checkboxes, and inputs validation

--- src/components/EditMovie.js
+++ src/components/EditMovie.js
@@ -3,6 +3,8 @@ import { useNavigate, useOutletContext, useParams } from "react-router-dom";
 import Input from './form/Input'
 import Select from "./form/Select";
 import TextArea from "./form/TextArea";
+import CheckBox from "./form/Checkbox";
+import Swal from "sweetalert2";
 
 const EditMovie = () => {
     const navigate = useNavigate();
@@ -22,6 +24,7 @@ const EditMovie = () => {
     const hasError = (key) => {
         return errors.indexOf(key) !== -1;
     };
+
     const [movie, setMovie] = useState({
         id: 0,
         title: "",
@@ -29,20 +32,103 @@ const EditMovie = () => {
         runtime: "",
         mpaa_rating: "",
         description: "",
+        genres: [],
+        genres_array: [Array(13).fill(false)],
     });
 
     // get id from the URL
     let { id } = useParams();
+    if (id === undefined) {
+        id = 0;
+    }
 
     useEffect(() => {
         if (jwtToken === "") {
             navigate("/login");
             return;
         }
-    }, [jwtToken, navigate]);
+
+        if (id === 0) {
+            // adding a movie
+            setMovie({
+                id: 0,
+                title: "",
+                release_date: "",
+                runtime: "",
+                mpaa_rating: "",
+                description: "",
+                genres: [],
+                genres_array: [Array(13).fill(false)],
+            });
+
+            const headers = new Headers();
+            headers.append("Content-Type", "application/json");
+
+            const requestOptions = {
+                method: "GET",
+                headers: headers,
+            };
+
+            fetch(`/genres`, requestOptions)
+                .then((response) => response.json())
+                .then((data) => {
+                    const checks = [];
+
+                    data.forEach(g => {
+                        checks.push({
+                            id: g.id,
+                            checked: false,
+                            genre: g.genre,
+                        });
+                    });
+
+                    setMovie(m => ({
+                        ...m,
+                        genres: checks,
+                        genres_array: [],
+                    }));
+                }).catch(error => {
+                    console.log(error);
+                });
+        } else {
+            // editing an existing movie
+        }
+    }, [id, jwtToken, navigate]);
 
     const handleSubmit = (event) => {
         event.preventDefault();
+
+        let errors = [];
+        let required = [
+            { field: movie.title, name: "title", },
+            { field: movie.release_date, name: "release_date", },
+            { field: movie.runtime, name: "runtime", },
+            { field: movie.description, name: "description", },
+            { field: movie.mpaa_rating, name: "mpaa_rating", },
+        ];
+
+        required.forEach(function (obj) {
+            if (obj.field === "") {
+                errors.push(obj.name);
+            }
+        });
+
+        if (movie.genres_array.length === 0) {
+            Swal.fire({
+                title: "Error!",
+                text: "You must choose at least one genre!",
+                icon: "error",
+                confirmButtonText: "ok",
+            });
+            errors.push("genres");
+        }
+
+        setErrors(errors);
+
+        if (errors.length > 0) {
+            return false;
+        }
+
     };
 
     const handleChange = () => (event) => {
@@ -54,11 +140,33 @@ const EditMovie = () => {
         });
     };
 
+    const handleCheck = (event, position) => {
+        console.log("handleCheck called");
+        console.log("value in handleCheck:", event.target.value);
+        console.log("checked is", event.target.checked);
+        console.log("position is", position);
+
+        let tmpArr = movie.genres;
+        tmpArr[position].checked = !tmpArr[position].checked;
+
+        let tmpIDs = movie.genres_array;
+        if (!event.target.checked) {
+            tmpIDs.splice(tmpIDs.indexOf(event.target.value));
+        } else {
+            tmpIDs.push(parseInt(event.target.value, 10));
+        }
+
+        setMovie({
+            ...movie,
+            genres_array: tmpIDs,
+        });
+    };
+
     return (
         <div>
             <h2>Add/Edit Movie</h2>
             <hr></hr>
-            <pre>{JSON.stringify(movie, null, 3)}</pre>
+            {/* <pre>{JSON.stringify(movie, null, 3)}</pre> */}
             <form onSubmit={handleSubmit}>
                 <input type="hidden" name="id" value={movie.id} id="id"></input>
                 <Input
@@ -100,7 +208,7 @@ const EditMovie = () => {
                     options={mpaaOptions}
                     onChange={handleChange("mpaa_rating")}
                     placeHolder={"Choose..."}
-                    errorMsg={"Please choose"}
+                    errorMsg={"Please choose a rating"}
                     errorDiv={hasError("mpaa_rating") ? "text-danger" : "d-none"}
                 />
 
@@ -113,6 +221,30 @@ const EditMovie = () => {
                     errorMsg={"Please enter a description"}
                     errorDiv={hasError("description") ? "text-danger" : "d-none"}
                 />
+
+                <hr></hr>
+
+                <h3>Genres</h3>
+
+                {movie.genres && movie.genres.length > 1 &&
+                    <>
+                        {Array.from(movie.genres).map((g, index) =>
+                            <CheckBox
+                                title={g.genre}
+                                name={"genre"}
+                                key={index}
+                                id={"genre-" + index}
+                                onChange={(event) => handleCheck(event, index)}
+                                value={g.id}
+                                checked={movie.genres[index].checked}
+                            />
+                        )}
+                    </>
+                }
+
+                <hr></hr>
+
+                <button className="btn btn-primary">Save</button>
             </form>
         </div>
     );
